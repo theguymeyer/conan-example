71cd0a2edcf5e80503d52a63545a81812f43648c
Guy Meyer <guymeyer1995@gmail.com>
2025-02-08 13:38:53 +0200

    add conan install script

--- app/conan-install.sh
+++ app/conan-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # the reason for the compiler and version hardcoding is because this conan system does not recognize my updated clang version of 15
 
-conan create . fib/1.1@gmeyer/testing  -s compiler="apple-clang" -s compiler.version="14.0"
+conan install . -s compiler="apple-clang" -s compiler.version="14.0"
